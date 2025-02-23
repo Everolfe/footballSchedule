437c8e2eed7a9413737fc4d56e38f29dfdfcc55e
Everolfe <rusakgeorgy@gmail.com>
2025-02-23 16:08:49 +0300

    Add files via upload

---
+++ src/test/java/com/github/everolfe/footballmatches/FootballMatchesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.everolfe.footballmatches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballMatchesApplicationTests {
+
+    @Test
+    void contextLoads() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
