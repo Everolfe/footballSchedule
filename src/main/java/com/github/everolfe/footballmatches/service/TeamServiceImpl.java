5e86ef94c27a4bc46ac3564427a17161cf2c7601
Everolfe <rusakgeorgy@gmail.com>
2025-02-23 18:41:42 +0300

    Update TeamServiceImpl.java

--- src/main/java/com/github/everolfe/footballmatches/service/TeamServiceImpl.java
+++ src/main/java/com/github/everolfe/footballmatches/service/TeamServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 
