b064efb5cd16d0a37213847bf34c10a23fe7ae1f
Everolfe <rusakgeorgy@gmail.com>
2025-03-05 11:04:57 +0300

    update rep and make cache

--- src/main/java/com/github/everolfe/footballmatches/cache/Cache.java
+++ src/main/java/com/github/everolfe/footballmatches/cache/Cache.java
@@ -1,12 +1,12 @@
 package com.github.everolfe.footballmatches.cache;
 
-import org.springframework.stereotype.Component;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.springframework.stereotype.Component;
+
 
 @Component
-public class Cache<K,V> extends LinkedHashMap<K,V> {
+public class Cache<K, V> extends LinkedHashMap<K, V> {
 
     @Override
     protected boolean removeEldestEntry(final Map.Entry<K, V> eldest) {
--- src/main/java/com/github/everolfe/footballmatches/repository/MatchRepository.java
+++ src/main/java/com/github/everolfe/footballmatches/repository/MatchRepository.java
@@ -1,9 +1,8 @@
 package com.github.everolfe.footballmatches.repository;
 
 import com.github.everolfe.footballmatches.model.Match;
-import java.util.List;
-
 import com.github.everolfe.footballmatches.model.Team;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
