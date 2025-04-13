83eee9be0ce66e70b7a6a01e2dfd7a5606e9831f
Everolfe <rusakgeorgy@gmail.com>
2025-04-13 17:58:48 +0300

    changes

--- src/main/java/com/github/everolfe/footballmatches/logger/LogAsync.java
+++ src/main/java/com/github/everolfe/footballmatches/logger/LogAsync.java
@@ -2,7 +2,11 @@ package com.github.everolfe.footballmatches.logger;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.PosixFilePermission;
 import java.util.Map;
+import java.util.Set;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -17,8 +21,12 @@ public class LogAsync {
             String taskId, Map<String, String> taskStatus, Map<String, Path> taskFiles) {
         try {
             Thread.sleep(10000); // имитация долгой работы
-            Path file = Files.createTempFile("generated_log_" + taskId, ".log");
-            Files.writeString(file, "Лог-файл сгенерирован. ID: " + taskId);
+            Path tempDir = Path.of(System.getProperty("java.io.tmpdir"));
+            Path file = Files.createTempFile(tempDir, "generated_log_" + taskId, ".log");
+            Files.setPosixFilePermissions(file,
+                    Set.of(PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_WRITE));
+            Files.writeString(file, "Лог-файл сгенерирован. ID: " + taskId,
+                    StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
             taskFiles.put(taskId, file);
             taskStatus.put(taskId, "COMPLETED");
         } catch (InterruptedException e) {
