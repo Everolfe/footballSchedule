cd437c9490c0646dd9c71042cca705241fb71e80
Everolfe <rusakgeorgy@gmail.com>
2025-04-15 21:59:07 +0300

    Log Async

--- src/main/java/com/github/everolfe/footballmatches/logger/LogAsync.java
+++ src/main/java/com/github/everolfe/footballmatches/logger/LogAsync.java
@@ -6,7 +6,6 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import lombok.RequiredArgsConstructor;
